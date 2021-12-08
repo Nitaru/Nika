2d1a619d3d3a9dce02922cb449de987ddce3c2d9
Nitaru <nikoloztolordava10@gmail.com>
2021-12-08 19:17:49 +0400

    davalebani

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var saxeli = "nika"
+
+let gvari = "tolordava"
+
+let 
+
+const asaki = 10
+
+const ricxvi = 32 
+
+console.log(gvari , saxeli, asaki)
+
+console.log(asaki * ricxvi, saxeli ,gvari )
+
+console.log()
