b180eb58d41a9a789d5ee95f5874943af2ac387c
Nitaru <nikoloztolordava10@gmail.com>
2021-12-12 19:09:03 +0400

    saxeli

---
+++ js/function.js
@@ -0,0 +1,29 @@
+let ricxvi = 23;
+
+let kidericxvi = 34;
+
+let jami = ricxvi + kidericxvi;
+
+let gamosatani = "daachiret aq"
+
+let swori = true;
+
+let araswori = false;
+
+let amoucnobi = undefined;
+
+let masivi = [34 , "ricxvi" , "gamarjoba"];
+
+let obieqti = {
+    sisqe = 33, peri  = "44", wona = "87kg",
+    mitana = ["tbilisi", "qutaisi", "zugdidi"]
+};
+
+// let punqcia = function (){
+// };
+
+console.log(typeof(araswori))
+   
+console.log(amoucnobi, jami , swori, ricxvi)
+
+document.getElementById("useme").innerHTML = gamosatani;
