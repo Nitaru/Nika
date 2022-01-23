433059a6369f10dc6e76bb40c3912d9d917ca2db
Nitaru <nikoloztolordava10@gmail.com>
2022-01-23 19:11:29 +0400

    davalebab

--- js/javascript.js
+++ js/javascript.js
@@ -1,52 +1,18 @@
-// ========== drop and aside bar JS =================
 
-// let drop = document.querySelector('.drop')
-// let asidee = document.querySelector('aside')
-
-// drop.addEventListener("click", function(){
-//     this.classList.toggle('showed')
-//     asidee.classList.toggle('showed')
-// })
-
-
-// let dasacheri = document.querySelector('.togli')
-
-// let grapa = document.querySelector('.saweri')
-
-// dasacheri.addEventListener("click", function (){
-//     grapa.setAttribute('type', 'password')
-// })
-
-// ==================== btns JS ==================
-
-// let backdiv = document.querySelector('.btn-colors')
-// document.querySelectorAll('.btnrounded').forEach(els=>{
-//     els.addEventListener('click', function () {
-//         let color = this.style.backgroundColor
-//         backdiv.style.transition = "all .6s"
-//         backdiv.style.backgroundColor = color
+// document.querySelectorAll('.tabbtn').forEach(tab => {
+//     tab.addEventListener('click', function () {
+//         document.querySelector('.alltab button.active').classList.remove('active')
+//         document.querySelector('.mytab.active').classList.remove('active')
+//         this.classList.add('active')
+//         document.querySelector(this.getAttribute('data-id')).classList.add('active')
 //     })
-// })
+// });
 
+let firstcol = document.querySelector('.pirveli')
+let card1 = document.querySelector('.card1')
+let card2 = document.querySelector('.card2')
 
-// ============= password JS =====================
-
-// let togg = document.querySelector('#basic-addon1')
-// let togg2 = document.querySelector('#aka')
-// let tvali = document.querySelector('.bi')
-
-
-// togg.addEventListener('click' , function (){
-//     if (tvali.classList[1] == 'bi-eye-slash-fill') {
-//         tvali.style.color = 'blue'
-//         tvali.classList.replace('bi-eye-slash-fill', 'bi-eye-fill')
-//     }else{
-//         tvali.style.color = "grey"
-//         tvali.classList.replace('bi-eye-fill', 'bi-eye-slash-fill')
-//     }
-//     if (togg2.type == 'password') {
-//         togg2.type = 'text';
-//     }else {
-//         togg2.type = 'password'
-//     }
-// })
+firstcol.addEventListener('click', function () {
+    card1.classList.toggle('.changed')
+    card2.classList.toggle('.changed2')
+})
